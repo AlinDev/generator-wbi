dab7813063cfe0cb3c8a7238c82103f75caaecd4
Alin Bogdan <boaldeveloper@gmail.com>
2020-07-31 12:37:53 +0300

    redesigned the redux structure

---
+++ generators/redux/templates/errors/errorName.error.js
@@ -0,0 +1,17 @@
+
+
+export const manage<%= Name %>Errors = (error) => {
+  const errorString = JSON.stringify(error);
+  let messageID;
+  let pageId = " ";//fill it in
+  let fieldId = " ";//fill it in
+  switch (true) {
+    case errorString.includes("invalid_grant"):
+      messageID = l.invalid_user.id;
+      break;
+    default:
+      messageID = l.NEW_ERROR.id;
+      break;
+  }
+  return errorMessage(I18n.t(messageID), pageId, fieldId);
+};
