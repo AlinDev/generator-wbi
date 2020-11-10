1eb7377b011ce65427a5f70c476bfafbb473cbc3
Alin Bogdan <alin.bogdan@whiteboxideas.com>
2020-11-10 12:55:03 +0200

    added CamelCase

--- generators/app/index.js
+++ generators/app/index.js
@@ -30,13 +30,13 @@ module.exports = class extends Generator {
   }
 
   writing() {
-
     const path = this.options.path  ? `${this.options.path}/` : "";
     const _name = this.props.chapter;
+    const naMe= this.toCamelCaseString(_name.split(" "))
+    console.log("{index.js}[writing](37) naMe", naMe)
     const NAME = _name.toUpperCase();
     const name = _name.toLowerCase();
     const Name = name.charAt(0).toUpperCase() + name.slice(1);
-    console.log("path",path)
     this.composeWith(require.resolve("../page"), {
       path: `${path}${_name}` + "/pages",
       chapter: _name
@@ -49,4 +49,45 @@ module.exports = class extends Generator {
       { name, Name, NAME, _name }
     );
   }
+
+
+
+
+
+// convert the input array to camel case
+  toCamelCase(inputArray) {
+
+    let result = ""
+
+    for (let i = 0, len = inputArray.length; i < len; i++) {
+
+      let currentStr = inputArray[i]
+
+      let tempStr = currentStr.toLowerCase()
+
+      if (i != 0) {
+
+        // convert first letter to upper case (the word is in lowercase)
+        tempStr = tempStr.substr(0, 1).toUpperCase() + tempStr.substr(1)
+
+      }
+
+      result += tempStr
+
+    }
+
+    return result
+  }
+
+
+// this function call all other functions
+
+  toCamelCaseString(input) {
+
+    let words = input
+
+    return this.toCamelCase(words)
+
+  }
+
 };
