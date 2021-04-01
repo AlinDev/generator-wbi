96cbfb3a6faec65f76881e5a7628ccbd1d15af42
Alin Bogdan <alin.bogdan@whiteboxideas.com>
2021-04-01 10:49:25 +0300

    updates to redux hook

--- generators/rxjsTK/templates/useName.js
+++ generators/rxjsTK/templates/useName.js
@@ -4,8 +4,10 @@ import { <%= name %>Slice  } from "./<%= name %>.redux/<%= name %>.slice";
 
 export const useRedux<%= Name %> = () => {
     const dispatch = useDispatch();
-    const { accessToken } = useSelector((state) => state.core.access);
+    const { accessToken } = useReduxAccess();
     const {status:{fetching,isSuccessful,submitted}} = useSelector((state) =>  state. )
+
+    const { show } = useReduxLoading();
     const <%= name %> =  useCallback(( values ) => {
         dispatch(
             <%= name %>Slice.actions.fetch({
@@ -15,12 +17,20 @@ export const useRedux<%= Name %> = () => {
             })
         );
     },[dispatch,accessToken]);
+
     const <%= name %>Reset =  useCallback((  ) => {
     dispatch( <%= name %>Slice.actions.reset( ) );
     },[dispatch]);
+
     const <%= name %>Update =  useCallback(( key,value ) => {
     dispatch(  <%= name %>Slice.actions.update({key,value }) );
     },[dispatch]);
-    return { <%= name %>,<%= name %>Reset,<%= name %>Update,fetching,isSuccessful,submitted };
+
+
+    const showLoading = useCallback(() => {
+      show("<%= name %>");
+    }, [dispatch]);
+
+    return {showLoading, <%= name %>,<%= name %>Reset,<%= name %>Update,fetching,isSuccessful,submitted };
 };
 
